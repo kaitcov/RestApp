878d8e05e1252067a694dad25867804e86eb025e
Kaitlin Coventry-Cox <kaitlincov@gmail.com>
2020-05-19 20:56:26 -0400

    First Commit

---
+++ src/main/java/com/kaitlin/restservice/RestApp/RestAppApplication.java
@@ -0,0 +1,14 @@
+package com.kaitlin.restservice.RestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(RestAppApplication.class, args);
+	}
+
+}
